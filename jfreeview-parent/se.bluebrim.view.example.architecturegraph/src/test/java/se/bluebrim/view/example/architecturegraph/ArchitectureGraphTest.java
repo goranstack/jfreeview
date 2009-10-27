257e4ae471f758a0a6bb7a133f08f5a808263d9f
goran.stack <goran.stack@37bab2c8-a04e-11de-9858-1998a7f0ce10>
2009-10-27 13:47:29 +0000

    Added unit test

---
+++ jfreeview-parent/se.bluebrim.view.example.architecturegraph/src/test/java/se/bluebrim/view/example/architecturegraph/ArchitectureGraphTest.java
@@ -0,0 +1,14 @@
+package se.bluebrim.view.example.architecturegraph;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class ArchitectureGraphTest {
+
+	@Test
+	public void testMain() throws IOException {
+		ArchitectureGraph.main(null);
+	}
+
+}
