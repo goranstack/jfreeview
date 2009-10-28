62810a715762742b033bb366991750631765b65b
goran.stack <goran.stack@37bab2c8-a04e-11de-9858-1998a7f0ce10>
2009-10-28 09:44:57 +0000

    Added unit test and jnlp folder to SVGRasterizer Test

---
+++ trunk/jfreeview-parent/se.bluebrim.view.example.svg.rasterizer/src/test/java/se/bluebrim/view/example/svg/rasterizer/SVGRasterizerTestTest.java
@@ -0,0 +1,15 @@
+package se.bluebrim.view.example.svg.rasterizer;
+
+import java.io.IOException;
+
+import org.apache.batik.transcoder.TranscoderException;
+import org.junit.Test;
+
+public class SVGRasterizerTestTest {
+
+	@Test
+	public void testMain() throws IOException, TranscoderException {
+		SVGRasterizerTest.main(null);
+	}
+
+}
