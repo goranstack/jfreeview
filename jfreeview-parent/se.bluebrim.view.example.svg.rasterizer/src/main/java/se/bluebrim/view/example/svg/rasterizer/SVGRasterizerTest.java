5775e8ed53555c66c1a0b8e829804a67dae65075
goran.stack <goran.stack@37bab2c8-a04e-11de-9858-1998a7f0ce10>
2009-10-28 09:44:57 +0000

    Added unit test and jnlp folder to SVGRasterizer Test

--- jfreeview-parent/se.bluebrim.view.example.svg.rasterizer/src/main/java/se/bluebrim/view/example/svg/rasterizer/SVGRasterizerTest.java
+++ jfreeview-parent/se.bluebrim.view.example.svg.rasterizer/src/main/java/se/bluebrim/view/example/svg/rasterizer/SVGRasterizerTest.java
@@ -5,12 +5,23 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import org.apache.batik.transcoder.TranscoderException;
 
 import se.bluebrim.view.batik.SVGRasterizer;
 
+/**
+ * TODO:
+ * <ul>
+ * <li>Add background square pattern like Photoshop and make sure the transparent elements
+ * in the SVG are rendered correctly </li>
+ * <li>Let the user try other SVG files by dropping them in to the window </li> 
+ * </ul>
+ * @author Goran Stack
+ *
+ */
 public class SVGRasterizerTest
 {
 
@@ -19,10 +30,11 @@ public class SVGRasterizerTest
       SVGRasterizer rasterizer = new SVGRasterizer(SVGRasterizerTest.class.getResource("test.svg"));
       rasterizer.setBackgroundColor(Color.WHITE);
       BufferedImage img = rasterizer.createBufferedImage();
-      javax.swing.JFrame f = new javax.swing.JFrame();
-      f.getContentPane().add(new JLabel(new ImageIcon(img)), BorderLayout.CENTER);  f.pack();
-      f.setVisible(true);
- 
+      JFrame window = new JFrame();
+      window.getContentPane().add(new JLabel(new ImageIcon(img)), BorderLayout.CENTER);
+      window.setSize(400, 400);
+      window.setLocation(200, 200);
+      window.setVisible(true);
 	}
 
 }
